b952e059c05211894de2724421d62fa08624f5df
Chi Shing <chishingt@outlook.com>
2017-05-20 09:56:40 +0800

    Completed project

--- 11.RetroCalcExercise/ViewController.swift
+++ 11.RetroCalcExercise/ViewController.swift
@@ -10,16 +10,79 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBOutlet weak var outputLabel: UILabel!
+
+    var leftVariable = ""
+    var rightVariable = ""
+    var runningNumber = ""
+    var result = ""
+    var currentOperation = ""
+    
+    @IBAction func buttonPressed (btn:UIButton!){
+        runningNumber += "\(btn.tag)"
+        outputLabel.text = runningNumber
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func clearPressed (op:UIButton!){
+        runningNumber = ""
+        leftVariable = ""
+        rightVariable = ""
+        result = ""
+        currentOperation = ""
+        outputLabel.text = runningNumber
     }
-
-
+    
+    @IBAction func addPressed (op:UIButton!){
+            processOperation(op:"Add")
+    }
+    
+    @IBAction func minusPressed (op:UIButton!){
+            processOperation(op:"Minus")
+    }
+    
+    @IBAction func multiplyPressed (op:UIButton!){
+            processOperation(op:"Multiply")
+    }
+    
+    @IBAction func dividePressed (op:UIButton!){
+            processOperation(op:"Divide")
+    }
+    
+    @IBAction func equalPressed (op:UIButton!){
+        processOperation(op:currentOperation)
+    }
+    
+    func processOperation (op:String){
+        if currentOperation != "" {
+            rightVariable = runningNumber
+            
+            if runningNumber != ""{
+                if currentOperation == "Add" {
+                    result = "\((Double(leftVariable)! + Double(rightVariable)!))"
+                }
+                else if currentOperation == "Minus" {
+                    result = "\((Double(leftVariable)! - Double(rightVariable)!))"
+                }
+                else if currentOperation == "Multiply" {
+                    result = "\((Double(leftVariable)! * Double(rightVariable)!))"
+                }
+                else if currentOperation == "Divide" {
+                    result = "\((Double(leftVariable)! / Double(rightVariable)!))"
+                }
+            }
+            currentOperation = op
+            outputLabel.text = result
+            leftVariable = result
+            runningNumber = ""
+        }
+        else {
+            leftVariable = runningNumber
+            currentOperation = op
+            runningNumber = ""
+        }
+        
+    }
+    
+    
 }
 
